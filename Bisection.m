784dd026169c4401bc85814e429a04f1d06dcc12
Sania7 <42738243+Sania7@users.noreply.github.com>
2019-10-29 23:04:10 +0600

    Create Bisection.m

---
+++ Bisection.m
@@ -0,0 +1,14 @@
+function retval = Bisection(f,a,b);
+  f = inline(f);
+  d = 1e-10;
+  while b-a >= d
+    c = (a+b)/2;
+    printf("%d %d %d %d %d %d\n", a, b, f(a), f(b), c, f(c));
+    if f(c)*a < 0
+      b = c;
+    else
+      a = c;
+    endif
+  endwhile
+  value = c;
+endfunction
